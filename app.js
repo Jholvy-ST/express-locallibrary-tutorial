4ef82f261e720c0b5e7280e9ea6c427b1b7d61b1
Jholvy Sarmiento Torres <jholvysarmiento@gmail.com>
2022-04-14 15:07:54 -0500

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ var helmet = require('helmet');
 
 var app = express();
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://test:testLibrary@cluster0.fnant.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://test:testLibrary@cluster0.fnant.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
